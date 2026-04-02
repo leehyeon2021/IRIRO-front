edd33099dc9910ee72aa67ce45015088f41c22fa
kbm1611 <bm1611@naver.com>
2026-04-02 15:23:49 +0900

    정렬해서 리스트 가져오기

--- src/main/react/src/api/tmapPOI.js
+++ src/main/react/src/api/tmapPOI.js
@@ -4,29 +4,18 @@ import axios from 'axios';
 // 🔍 Tmap POI 검색 API 호출 함수
 export const searchTmapPOI = async (keyword) => {
     try {
-        const response = await axios.get("https://apis.openapi.sk.com/tmap/pois", {
-            params: {
-                version: 1,
-                searchKeyword: keyword,
-                format: "json",
-                count: 15, // 최대 15개까지 가져오기
-            },
+        const response = await 
+        axios.get(`https://apis.openapi.sk.com/tmap/pois?version=1&searchKeyword=${keyword}&format=json&count=15&sort=score`, {
             headers: {
-                // ⭐ Vite 전용 환경변수 호출 방식!
                 appKey: import.meta.env.VITE_TMAP_JS_KEY,
             },
         });
-
-        console.log("티맵 키값 확인:", import.meta.env.VITE_TMAP_JS_KEY);
-        // 티맵 응답 구조에서 실제 장소 배열(poi)만 쏙 뽑아냅니다.
         const poiData = response.data?.searchPoiInfo?.pois?.poi;
-
-        // 검색 결과가 있으면 배열을, 없으면 빈 배열을 반환
-        return poiData || [];
+        if(poiData) return poiData;
+        else return [];
 
     } catch (error) {
         console.error("Tmap POI API 호출 중 에러 발생:", error);
-        // 에러가 나면 화면(컴포넌트) 쪽에서 알 수 있게 에러를 던져줍니다.
         throw error;
     }
 };
