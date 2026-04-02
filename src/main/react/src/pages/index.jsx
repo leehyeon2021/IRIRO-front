e66e8dc260e0ec9a31a71fcfe1840dce29c0c58b
so0hub <so0mile02@gmail.com>
2026-04-02 18:15:28 +0900

    community 라우트 추가

--- src/main/react/src/pages/index.jsx
+++ src/main/react/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './map/MainPage.jsx';
+import CommunityMain from './community/CommunityMain.jsx';
 
 
 export default function index(props){
@@ -7,7 +8,7 @@ export default function index(props){
     {/* <Header></Header> 추후에 헤더들어감 -> 왼쪽 위 로그(이리로)*/}
     <Routes>
         <Route path='/' element={< MainPage />}></Route> {/* 메인페이지 */}
-        <Route path='/' element={< CommunityMain />}></Route>
+        <Route path='/community' element={< CommunityMain />}></Route>
         {/* 밑에 계속 추가 */}
     </Routes>
     {/* <Footer></Footer> 추후에 푸터들어감 */}
