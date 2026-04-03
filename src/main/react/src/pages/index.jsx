fba87e3c8513083e2b2d347cf5e3ba7d93212c28
so0hub <so0mile02@gmail.com>
2026-04-03 15:54:55 +0900

    헤더 추가

--- src/main/react/src/pages/index.jsx
+++ src/main/react/src/pages/index.jsx
@@ -5,15 +5,16 @@ import Login from './community/Login.jsx';
 
 
 export default function index(props){
-    return(<>
-    {/* <Header></Header> 추후에 헤더들어감 -> 왼쪽 위 로그(이리로)*/}
+    return(
+        <div id="wrap">
+            <Header/>
     <Routes>
         <Route path='/' element={< MainPage />}></Route> {/* 메인페이지 */}
         <Route path='/community' element={< CommunityMain />}></Route> {/* 커뮤니티 메인페이지 */}
          <Route path='/login' element={< Login />}></Route> {/* 로그인 페이지 */}
-         
+
         {/* 밑에 계속 추가 */}
     </Routes>
     {/* <Footer></Footer> 추후에 푸터들어감 */}
-    </>)
+    </div>)
 }
