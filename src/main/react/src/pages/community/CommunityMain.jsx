633c24015d9077e6a68b21a5b4bb63adf9ad3cbc
so0hub <so0mile02@gmail.com>
2026-04-03 15:50:47 +0900

    CommunityMain 커뮤메인페이지 수정

--- src/main/react/src/pages/community/CommunityMain.jsx
+++ src/main/react/src/pages/community/CommunityMain.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const CommunityMain = () => {
-    return <div>CommunityMain</div>;
+    return( <div>
+        <h2>이리로 메인페이지</h2>
+        <div> 로그인 </div>
+    
+        
+        
+        
+
+        </div>)
 };
 export default CommunityMain;
