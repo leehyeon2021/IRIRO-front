f5ba911c1e4e431ef8d3a5b5caab0c4aedfeff6a
so0hub <so0mile02@gmail.com>
2026-04-03 15:51:29 +0900

    커뮤메인페이지 , 로그인페이지 라우터 추가

--- src/main/react/src/pages/index.jsx
+++ src/main/react/src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './map/MainPage.jsx';
 import CommunityMain from './community/CommunityMain.jsx';
+import Login from './community/Login.jsx';
 
 
 export default function index(props){
@@ -8,7 +9,9 @@ export default function index(props){
     {/* <Header></Header> 추후에 헤더들어감 -> 왼쪽 위 로그(이리로)*/}
     <Routes>
         <Route path='/' element={< MainPage />}></Route> {/* 메인페이지 */}
-        <Route path='/community' element={< CommunityMain />}></Route>
+        <Route path='/community' element={< CommunityMain />}></Route> {/* 커뮤니티 메인페이지 */}
+         <Route path='/login' element={< Login />}></Route> {/* 로그인 페이지 */}
+         
         {/* 밑에 계속 추가 */}
     </Routes>
     {/* <Footer></Footer> 추후에 푸터들어감 */}
