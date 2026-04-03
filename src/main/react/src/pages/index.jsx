8276a57243baf8e909bd5aea8e0f117f83d2e747
leehyeon2021 <leehyeon2021@gmail.com>
2026-04-03 16:28:41 +0900

    260403 index.jsx에 ArticleMain 추가

--- src/main/react/src/pages/index.jsx
+++ src/main/react/src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './map/MainPage.jsx';
 import CommunityMain from './community/CommunityMain.jsx';
+import ArticleMain from './article/ArticleMain.jsx';
 
 
 export default function index(props){
@@ -9,6 +10,7 @@ export default function index(props){
     <Routes>
         <Route path='/' element={< MainPage />}></Route> {/* 메인페이지 */}
         <Route path='/community' element={< CommunityMain />}></Route>
+        <Route path='/article' element={ <ArticleMain/>}></Route>
         {/* 밑에 계속 추가 */}
     </Routes>
     {/* <Footer></Footer> 추후에 푸터들어감 */}
