424fbf29e423e89b0e170fb9c2f6878e175a3e88
leehyeon2021 <leehyeon2021@gmail.com>
2026-04-05 21:02:56 +0900

    260405 기사 지역구 검색 페이지

--- src/main/react/src/pages/article/ArticleList.jsx
+++ src/main/react/src/pages/article/ArticleList.jsx
@@ -1,8 +1,45 @@
+import logo from '../../assets/logo_iriro.png'
+import ArticleSearch from "./ArticleSearch.jsx";
 
 export default function ArticleList( props ){
     return(<>
         <div>
-            
+            <div>
+                <img src={logo} name={logo}/>
+                <div> 우리 지역구 안전/범죄 기사 </div>
+            </div>
+            <form onSubmit={ArticleSearch}>
+                <div> 서울 특별시 지역구 검색 -> </div>
+                <select className={seoulDistrict}>
+                    <option>전체</option>
+                    <option>강남구</option>
+                    <option>강동구</option>
+                    <option>강북구</option>
+                    <option>강서구</option>
+                    <option>관악구</option>
+                    <option>광진구</option>
+                    <option>구로구</option>
+                    <option>금천구</option>
+                    <option>노원구</option>
+                    <option>도봉구</option>
+                    <option>동대문구</option>
+                    <option>동작구</option>
+                    <option>마포구</option>
+                    <option>서대문구</option>
+                    <option>서초구</option>
+                    <option>성동구</option>
+                    <option>성북구</option>
+                    <option>송파구</option>
+                    <option>양천구</option>
+                    <option>영등포구</option>
+                    <option>용산구</option>
+                    <option>은평구</option>
+                    <option>종로구</option>
+                    <option>중구</option>
+                    <option>중랑구</option>
+                </select>
+                <button type="submit">🔎</button>
+            </form>
         </div>
     </>)
 }
