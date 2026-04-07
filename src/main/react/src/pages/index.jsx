418c044b067aa360b4cc7017cfdaf4b41032de31
so0hub <so0mile02@gmail.com>
2026-04-07 17:22:40 +0900

    라우트 삭제

--- src/main/react/src/pages/index.jsx
+++ src/main/react/src/pages/index.jsx
@@ -2,9 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import MainPage from './map/MainPage.jsx';
 import CommunityMain from './community/CommunityMain.jsx';
 import ArticleMain from './article/ArticleMain.jsx';
-import Login from './community/Login.jsx';
 import Header from '../components/layout/Header.jsx';
-import Write from './community/Write.jsx';
 
 
 export default function index(props){
@@ -13,10 +11,8 @@ export default function index(props){
             <Header/>
     <Routes>
         <Route path='/' element={< MainPage />}></Route> {/* 메인페이지 */}
-        <Route path='/community' element={< CommunityMain />}></Route>
+        <Route path='/community' element={< CommunityMain />}></Route> {/* 커뮤니티 메인페이지 */}
         <Route path='/article' element={ <ArticleMain/>}></Route>
-        <Route path='/login' element={< Login />}></Route> {/* 로그인 페이지 */}
-        <Route path='/write' element={< Write />}></Route> {/* 글쓰기 페이지 */}
 
         {/* 밑에 계속 추가 */}
     </Routes>
