f2adf24d4ef46cbe71c4bfd2285a91e6f634f47a
leehyeon2021 <leehyeon2021@gmail.com>
2026-04-07 18:23:10 +0900

    260407 기사 게시판 - css 추가

--- src/main/react/src/pages/article/ArticleHeader.jsx
+++ src/main/react/src/pages/article/ArticleHeader.jsx
@@ -1,9 +1,9 @@
-import { Link, Route, Routes } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import '../../css/article/ArticleHeader.css';
 
 export default function ArticleNavi( props ){
     return(<>
-            <div>
+            <div className="header-sticky">
                 <Link to="/"> ↩️ 지도로 돌아가기 </Link>
                 <Link to="/community"> 👥 이리로 커뮤니티 </Link>
             </div>
