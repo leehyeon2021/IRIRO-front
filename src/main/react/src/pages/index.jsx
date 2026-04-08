cd8da3a568fc1f1ac14eff5ac8ee5d9d4bbee405
so0hub <so0mile02@gmail.com>
2026-04-08 17:38:31 +0900

    회원가입 라우터 추가

--- src/main/react/src/pages/index.jsx
+++ src/main/react/src/pages/index.jsx
@@ -9,15 +9,13 @@ import ArticleMain from "./article/ArticleMain.jsx";
 
 // 2. 하위 경로에서 사용할 Login과 Write도 여기서 가져와야 합니다.
 import Login from "./community/Login.jsx";
+import Sign from "./community/Sign.jsx";
 import Write from "./community/Write.jsx";
 
-// Header 경로 확인 (상위 폴더로 나갔다 들어가는 경로)
-import Header from "../components/layout/Header.jsx";
 
 export default function Index(props) {
     return (
         <div id="wrap">
-            <Header />
             <Routes>
                 <Route element={<MainLayout />}>
                     <Route path='/' element={< MainPage />}></Route> {/* 메인페이지 */}
@@ -25,6 +23,7 @@ export default function Index(props) {
                     <Route path='/community' element={<CommunityMain />}>
                         <Route index element={<CommunityList />} />
                         <Route path='login' element={<Login />} />
+                        <Route path='sign' element={<Sign />} />
                         <Route path='write' element={<Write />} />
                     </Route>
                     <Route path='/article' element={<ArticleMain />} />
