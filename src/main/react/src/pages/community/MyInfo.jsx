67932176ce0b13fcdc8ea663df48b852d12db153
so0hub <so0mile02@gmail.com>
2026-04-13 10:13:57 +0900

    마이페이지 뒤로가기 추가

--- src/main/react/src/pages/community/MyInfo.jsx
+++ src/main/react/src/pages/community/MyInfo.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export default function MyInfo(props){
@@ -32,6 +33,7 @@ export default function MyInfo(props){
 
     <div>
         <h2> 마이페이지 </h2>
+        <Link to="/community"> 뒤로가기 </Link> 
         <table>
             <thead>
                 <tr>
